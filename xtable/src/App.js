9210e35c60da19184bd1f657ba06f6dbb424ae2b
VENKAT <131847253+BVENKATASRAVANAKUMAR@users.noreply.github.com>
2025-12-19 19:34:07 +0530

    Updated commit

---
+++ xtable/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+
+const initialData = [
+  { date: "2022-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-01", views: 100, article: "Article 1" },
+  { date: "2023-09-02", views: 150, article: "Article 2" },
+  { date: "2023-09-02", views: 120, article: "Article 3" },
+  { date: "2020-09-03", views: 200, article: "Article 4" }
+];
+
+function App() {
+  const [data, setData] = useState(initialData);
+  const sortByDate = () => {
+    const sorted = [...data].sort(
+      (a, b) => new Date(b.date) - new Date(a.date)
+    );
+    setData(sorted);
+  };
+
+  const sortByViews = () => {
+    const sorted = [...data].sort((a, b) => b.views - a.views);
+    setData(sorted);
+  };
+  return (
+       <div>
+      <h1>Date and Views Table</h1>
+      <div class="btn">
+      <button  onClick={sortByDate}>Sort by Date</button>
+      <button onClick={sortByViews}>Sort by Views</button>
+      </div>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.date}</td>
+              <td>{item.views}</td>
+              <td>{item.article}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
